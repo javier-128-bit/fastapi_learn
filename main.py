a51cd4586e60db9d3a3b3e698fef84e4096bad61
javier <gamingjavi21@gmail.com>
2026-04-28 09:43:42 +0700

    POST

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
+from typing import Optional
 
-application=FastAPI()
+application = FastAPI()
 
+data_transaction = []
+
+class Tipe(str, Enum):
+    INCOME = "INCOME"
+    PURCHASE = "PURCHASE"
+    INVEST = "INVEST"
+
+class Method(str, Enum):
+    BANK = "BANK"
+    CASH = "CASH"
+    QRIS = "QRIS"
+
+class InputType(BaseModel):
+    tipe: Tipe
+    amount: int
+    notes: Optional[str] = None
+    method: Method
+
+# Query Parameter
 @application.get("/transaction")
-def get_transaction(tipe:str,amount:int):
-    return f"tipe ini adalah {tipe} dan jumlahnya {amount}" 
+def get_transaction(tipe: str, amount: int):
+    return {
+        "tipe": tipe,
+        "amount": amount
+    }
 
-#PATH
+# Path Parameter
 @application.get("/transaction/{tipe}")
-def get_transaction(tipe):
-    return f"Tipe ku adalah {tipe}"
+def get_tipe(tipe: str):
+    return {
+        "tipe": tipe
+    }
 
+# POST
+@application.post("/transaction")
+def insert_transaction(data: InputType):
+    data_transaction.append(data.dict())
+    return {
+        "message": "berhasil ditambahkan",
+        "data": data
+    }
